923f4383c7f37394fd50e92f1c5f0e3ed92f07dd
saidvrworld <saidhusan.saidvaliev@gmail.com>
2023-10-10 15:03:12 +0300

    Added templates

--- onlineshop/urls.py
+++ onlineshop/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from . import views
-app_name = "packages"
+app_name = "onlineshop"
 
 urlpatterns = [
 
     path("", views.index, name="main_page"),
     path("add_bottle", views.addBottle, name="add_bottle"),
+    path("create_bottle/", views.createBottle, name="create_bottle"),
+    path("all_products/", views.get_product_list, name="all_products"),
 
 ]
--- onlineshop/views.py
+++ onlineshop/views.py
@@ -2,13 +2,33 @@ from django.shortcuts import render
 from .models import *
 def index(request):
     return render(request, "onlineshop/main.html")
+
 def addBottle(request):
     return render(request, "onlineshop/add_bottle.html")
 
+def thank_you(request):
+    return render(request, "onlineshop/thank_you.html")
 
-#create package objects and send you to "you have successfuly created package" page
+def get_product_list(request):
+    product_list = Bottle.objects.all()
+    context = {"product_list":product_list}
+    return render(request, "onlineshop/Products.html",context)
+
+
+#create Bottle objects
 def createBottle(request):
-    package_name = request.POST["name"]
+    bottle_code = request.POST["bottle_code"]
+    main_type = request.POST["main_type"]
+    sub_type1 = request.POST["sub_type1"]
+    weigth = request.POST["weigth"]
+    width = request.POST["width"]
+    height = request.POST["height"]
+    neck_size = request.POST["neck_size"]
+    volume = request.POST["volume"]
 
-    bottle = Bottle(name=,category=,sub_category,volume,neck_size,weight,height,width)
-    return render(request, "packages/package_added.html",{"id":bottle})
+
+    bottle = Bottle(name=bottle_code,category=main_type,sub_category=sub_type1,volume=volume,neck_size=neck_size,
+                    weight = weigth,height = height,width=width)
+    bottle.save()
+
+    return thank_you(request)
